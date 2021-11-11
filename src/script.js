fd6adf5d01b651156dd656b13717a158e2acfbe3
Abolfazl-drx <abolfazl_drx@yahoo.com>
2021-11-11 12:32:42 -0800

    night mode added; instruction-one added;

--- src/script.js
+++ src/script.js
@@ -1,2 +1,25 @@
-const ho = 'hi'
-console.log(ho)
+function toggleTheme_colors() {
+    let theme = document.getElementsByTagName('link')[0];
+    if (theme.getAttribute('href') === 'res/css/stylesheet(light-mode).css') {
+        theme.setAttribute('href', 'res/css/stylesheet(dark-mode).css');
+    } else {
+        theme.setAttribute('href', 'res/css/stylesheet(light-mode).css');
+    }
+}
+function toggleTheme_background(){
+    let background = document.getElementsByClassName('head-background')[0];
+    if (background.getAttribute('src') === 'res/images/bg-desktop-light.jpg') {
+        background.setAttribute('src', 'res/images/bg-desktop-dark.jpg');
+    } else {
+        background.setAttribute('src', 'res/images/bg-desktop-light.jpg');
+    }
+}
+
+function toggleTheme_icon(){
+    let icon = document.getElementsByClassName('header-toggle_icon')[0];
+    if (icon.getAttribute('src') === 'res/images/icon-moon.svg') {
+        icon.setAttribute('src', 'res/images/icon-sun.svg');
+    } else {
+        icon.setAttribute('src', 'res/images/icon-moon.svg');
+    }
+}
